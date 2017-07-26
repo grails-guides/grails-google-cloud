a7499365bc79bd4538ca1a4a0aebbff67c46d275
sdelamo <sergio.delamo@softamo.com>
2017-07-26 10:44:16 +0200

    Add BookController code

---
+++ initial/grails-app/controllers/demo/BookController.groovy
@@ -0,0 +1,182 @@
+// tag::packageImport[]
+package demo
+
+import grails.gorm.transactions.Transactional
+import groovy.transform.CompileDynamic
+import groovy.transform.CompileStatic
+import org.springframework.context.MessageSource
+
+import java.awt.print.Book
+
+import static org.springframework.http.HttpStatus.*
+
+// end::packageImport[]
+// tag::imports[]
+// end::imports[]
+// tag::classDeclaration[]
+@SuppressWarnings('LineLength')
+@CompileStatic
+class BookController {
+// end::classDeclaration[]
+
+  // tag::properties[]
+    static allowedMethods = [
+            index: 'GET',
+            show: 'GET',
+            edit: 'GET',
+            create: 'GET',
+            save: 'POST',
+            update: 'PUT',
+            delete: 'DELETE',
+    ]
+
+    BookGormService bookGormService
+
+    MessageSource messageSource
+    // end::properties[]
+
+    // tag::actionIndex[]
+    @CompileDynamic
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        def (l, total) = bookGormService.list(params)
+        respond l, model: [bookCount: total]
+    }
+    // end::actionIndex[]
+
+    // tag::actionShow[]
+    @Transactional(readOnly = true)
+    def show(Book book) {
+        respond book
+    }
+    // end::actionShow[]
+
+    // tag::actionCreate[]
+    @SuppressWarnings(['FactoryMethodName', 'GrailsMassAssignment'])
+    @Transactional(readOnly = true)
+    def create() {
+        respond new Book(params)
+    }
+    // end::actionCreate[]
+
+    // tag::actionEdit[]
+    @Transactional(readOnly = true)
+    def edit(Book book) {
+        respond book
+    }
+    // end::actionEdit[]
+
+    // tag::actionSave[]
+    @CompileDynamic
+    def save(NameCommand cmd) {
+        if (cmd == null) {
+            notFound()
+            return
+        }
+
+        if (cmd.hasErrors()) {
+            respond cmd.errors, model: [book: cmd], view:'create'
+            return
+        }
+
+        def book = bookGormService.save(cmd)
+
+        if ( book == null ) {
+            notFound()
+            return
+        }
+
+        if ( book.hasErrors() ) {
+            respond book.errors, model: [book: book], view:'create'
+            return
+        }
+
+        request.withFormat {
+            form multipartForm {
+                String bookMessage = messageSource.getMessage('book.label', [] as Object[], 'Book', request.locale)
+                String message = messageSource.getMessage('default.created.message', [bookMessage, book.id] as Object[], 'Book Created', request.locale)
+                flash.message = message
+                redirect book
+            }
+            '*' { respond book, [status: CREATED] }
+        }
+    }
+    // end::actionSave[]
+
+    // tag::actionUpdate[]
+    @CompileDynamic
+    def update(NameUpdateCommand cmd) {
+        if (cmd == null) {
+            notFound()
+            return
+        }
+
+        if (cmd.hasErrors()) {
+            respond cmd.errors, model: [book: cmd], view: 'edit'
+            return
+        }
+
+        def book = bookGormService.update(cmd)
+
+        if ( book == null) {
+            notFound()
+            return
+        }
+
+        if ( book.hasErrors() ) {
+            respond book.errors, model: [book: book], view: 'edit'
+            return
+        }
+
+        request.withFormat {
+            form multipartForm {
+                String bookMessage = messageSource.getMessage('book.label', [] as Object[], 'Book', request.locale)
+                String message = messageSource.getMessage('default.updated.message', [bookMessage, book.id] as Object[], 'Book Updated', request.locale)
+                flash.message = message
+                redirect book
+            }
+            '*' { respond book, [status: OK] }
+        }
+    }
+    // end::actionUpdate[]
+
+    // tag::actionDelete[]
+    @CompileDynamic
+    def delete() {
+
+        Long bookId = params.long('id')
+
+        if ( !bookId ) {
+            notFound()
+            return
+        }
+
+        bookGormService.deleteById(bookId)
+
+        request.withFormat {
+            form multipartForm {
+                String bookMessage = messageSource.getMessage('book.label', [] as Object[], 'Book', request.locale)
+                String message = messageSource.getMessage('default.deleted.message', [bookMessage, bookId] as Object[], 'Book Deleted', request.locale)
+                flash.message = message
+                redirect action: 'index', method: 'GET'
+            }
+            '*' { render status: NO_CONTENT }
+        }
+    }
+    // end::actionDelete[]
+
+    // tag::notFound[]
+    @CompileDynamic
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                String bookMessage = messageSource.getMessage('book.label', [] as Object[], 'Book', request.locale)
+                String message = messageSource.getMessage('default.not.found.message', [bookMessage, params.id] as Object[], 'Book not Found', request.locale)
+                flash.message = message
+                redirect action: 'index', method: 'GET'
+            }
+            '*' { render status: NOT_FOUND }
+        }
+    }
+    // end::notFound[]
+}
---
+++ initial/grails-app/services/demo/BookGormService.groovy
@@ -0,0 +1,41 @@
+package demo
+
+import grails.gorm.transactions.Transactional
+import groovy.transform.CompileDynamic
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class BookGormService {
+
+    // tag::list[]
+    @Transactional(readOnly = true)
+    List list(Map params) {
+        [Book.list(params), Book.count()]
+    }
+    // end::list[]
+
+    // tag::save[]
+    @CompileDynamic
+    Book save(NameCommand cmd) {
+        def book = new Book()
+        book.properties = cmd.properties
+        book.save()
+    }
+    // end::save[]
+
+    // tag::update[]
+    @CompileDynamic
+    Book update(NameUpdateCommand cmd) {
+        Book book = Book.get(cmd.id)
+        book.properties = cmd.properties
+        book.save()
+    }
+    // end::update[]
+
+    // tag::deleteById[]
+    void deleteById(Long bookId) {
+        def book = Book.get(bookId)
+        book?.delete()
+    }
+    // end::deleteById[]
+}
