b7677ba87cdbc25dc8bf071cb7a062955abb0ee3
benrhine <rhine.ben@gmail.com>
2017-08-04 10:53:32 -0400

    upgrading gradle, gorm, grails to 3.3.0

--- complete/src/test/groovy/demo/BookSpec.groovy
+++ complete/src/test/groovy/demo/BookSpec.groovy
@@ -1,14 +1,13 @@
 package demo
 
-import grails.test.mixin.TestFor
+import grails.testing.gorm.DomainUnitTest
 import spock.lang.Specification
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @SuppressWarnings('MethodName')
-@TestFor(Book)
-class BookSpec extends Specification {
+class BookSpec extends Specification implements DomainUnitTest<Book> {
 
     void 'book name is not nullable'() {
         when:
